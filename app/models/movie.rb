5a3e730b9c2797a19dd58a828d7adb8cf81db758
Lauren Halenkamp <lhalenka@berkeley.edu>
2026-07-25 06:03:24 +0000

    fixing

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,17 @@
 class Movie < ApplicationRecord
-end
+  def self.all_ratings
+    ['G', 'PG', 'PG-13', 'R']
+  end
+  
+  def self.with_ratings(ratings_list)
+  # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all 
+    if ratings_list.present?
+      #movies with those ratings
+      Movie.where(rating: ratings_list)
+  # if ratings_list is nil, retrieve ALL movies
+    else
+      Movie.all
+    end
+  end
+  end
+
