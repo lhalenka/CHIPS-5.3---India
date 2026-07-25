385991634d75051bb46b71803b2896892b6e0604
Lauren Halenkamp <lhalenka@berkeley.edu>
2026-07-25 06:51:06 +0000

    workinnn

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,15 +3,15 @@ class Movie < ApplicationRecord
     ['G', 'PG', 'PG-13', 'R']
   end
   
-  def self.with_ratings(ratings_list)
+  def self.with_ratings(ratings_list, sort_by = '')
   # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all 
     if ratings_list.present?
       #movies with those ratings
-      Movie.where(rating: ratings_list)
+      Movie.where(rating: ratings_list).order(sort_by)
   # if ratings_list is nil, retrieve ALL movies
     else
-      Movie.all
+      Movie.all.order(sort_by)
     end
   end
-  end
+end
 
